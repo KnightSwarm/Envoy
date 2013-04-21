78a4048c5567f205e5d21a486084e059b84d7c99
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-04-21 02:21:01 +0200

    Add bootstrap for basic web-based panel

---
+++ src/panel/public_html/include/base.php
@@ -0,0 +1,37 @@
+<?php
+/* Copyright 2013 by Sven Slootweg <admin@cryto.net>
+ * 
+ * This file is part of Envoy.
+ * 
+ * Envoy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Envoy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Envoy.  If not, see <http://www.gnu.org/licenses/>. */
+
+if(!isset($_APP)) { die("Unauthorized."); }
+
+$_CPHP = true;
+$_CPHP_CONFIG = "../../config.json";
+require("cphp/base.php");
+
+function autoload_class($class_name) 
+{
+	global $_APP;
+	
+	$class_name = str_replace("\\", "/", strtolower($class_name));
+	
+	if(file_exists("classes/{$class_name}.php"))
+	{
+		require_once("classes/{$class_name}.php");
+	}
+}
+
+spl_autoload_register('autoload_class');
---
+++ src/panel/public_html/rewrite.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright 2013 by Sven Slootweg <admin@cryto.net>
+ * 
+ * This file is part of Envoy.
+ * 
+ * Envoy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Envoy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Envoy.  If not, see <http://www.gnu.org/licenses/>. */
+
+$_APP = true;
+
+require("include/base.php");
