860da0c0363a30179413f66782854f785472a5b9
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-05-15 01:06:15 +0200

    Add the basics of stanza processing and event handling to the component

--- src/envoyxmpp/component.py
+++ src/envoyxmpp/component.py
@@ -4,6 +4,8 @@ import sys
 
 import sleekxmpp
 from sleekxmpp.componentxmpp import ComponentXMPP
+from sleekxmpp.stanza import Message, Presence, Iq
+from sleekxmpp.xmlstream.matcher import MatchXPath, StanzaPath
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -11,7 +13,7 @@ sys.setdefaultencoding('utf8')
 class Component(ComponentXMPP):
 	def __init__(self, jid, host, port, password):
 		ComponentXMPP.__init__(self, jid, password, host, port)
-		self.add_event_handler("forwarded_stanza", self._handle_stanza)
+		self.add_event_handler("forwarded_stanza", self._envoy_handle_stanza)
 		
 		self.registerPlugin('xep_0030') # Service Discovery
 		self.registerPlugin('xep_0004') # Data Forms
@@ -19,5 +21,47 @@ class Component(ComponentXMPP):
 		self.registerPlugin('xep_0199') # XMPP Ping
 		self.registerPlugin('xep_0297') # Stanza forwarding
 		
-	def _handle_stanza(self, stanza):
-		print stanza
+		self._envoy_events = {}
+		
+	def _envoy_handle_stanza(self, wrapper):
+		stanza = wrapper['forwarded']['stanza']
+		#print stanza
+		
+		if isinstance(stanza, Iq):
+			self._envoy_handle_iq(wrapper, stanza)
+		elif isinstance(stanza, Message):
+			self._envoy_handle_message(wrapper, stanza)
+		elif isinstance(stanza, Presence):
+			self._envoy_handle_presence(wrapper, stanza)
+		else:
+			print "Unknown:", repr(stanza) # TODO: Log unknown stanza type
+	
+	def _envoy_handle_iq(self, wrapper, stanza):
+		if MatchXPath("{jabber:client}iq/{urn:xmpp:ping}ping").match(stanza):
+			self._envoy_call_event("ping", stanza["from"])
+		elif MatchXPath("{jabber:client}iq/{urn:ietf:params:xml:ns:xmpp-session}session").match(stanza):
+			self._envoy_call_event("login", stanza["from"])
+		return
+		
+	def _envoy_handle_message(self, wrapper, stanza):
+		print "Msg:", stanza
+		
+	def _envoy_handle_presence(self, wrapper, stanza):
+		if StanzaPath("presence@type=unavailable").match(stanza):
+			self._envoy_call_event("logout", stanza["from"], stanza["status"])
+		elif MatchXPath("{jabber:client}presence/{jabber:client}show").match(stanza):
+			self._envoy_call_event("status", stanza["from"], stanza["show"], stanza["status"])
+		else:
+			# Most likely the user switched to 'available'
+			self._envoy_call_event("status", stanza["from"], "available", stanza["status"])
+		return
+		
+	def _envoy_call_event(self, event_name, *args, **kwargs):
+		try:
+			self._envoy_events[event_name](*args, **kwargs)
+		except KeyError, e:
+			# No function has been registered for this event
+			pass
+		
+	def register_event(self, event_name, func):
+		self._envoy_events[event_name] = func
--- src/envoyxmpp/run_component.py
+++ src/envoyxmpp/run_component.py
@@ -1,5 +1,21 @@
 from component import Component
 
+def on_login(user):
+	print "%s just logged in." % user
+
+def on_logout(user, reason):
+	print "%s just disconnected with reason '%s'." % (user, reason)
+
+def on_ping(user):
+	print "%s just pinged." % user
+
+def on_status(user, status, message):
+	print "%s just changed their status to %s (%s)." % (user, status, message)
+
 xmpp = Component("component.envoy.local", "envoy.local", 5347, "password")
+xmpp.register_event("login", on_login)
+xmpp.register_event("logout", on_logout)
+xmpp.register_event("ping", on_ping)
+xmpp.register_event("status", on_status)
 xmpp.connect()
 xmpp.process(block=True)
