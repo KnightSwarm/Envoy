d56327df411f62d45a4c7fa0f6f16b7cdf9513fd
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-10-11 23:10:48 +0200

    Threads are evil. Also, first thread-safe queue implementation of SleekXMPP. Using Javascript. I must be crazy.

--- src/client/Envoy Client/Resources/script/ui.js
+++ src/client/Envoy Client/Resources/script/ui.js
@@ -38,4 +38,24 @@ envoyClient.controller('UiController', function UiController($scope)
 			"status": "offline"
 		}
 	];
+	
+	$scope.own_jid = "johndoe@fqdn.local";
+	
+	$scope.messages = [
+		{
+			"author_name": "John Doe",
+			"jid": "johndoe@fqdn.local",
+			"body": "Hi, testing"
+		},
+		{
+			"author_name": "John Doe",
+			"jid": "johndoe@fqdn.local",
+			"body": "Testing more..."
+		},
+		{
+			"author_name": "Jane Doe",
+			"jid": "janedoe@fqdn.local",
+			"body": "Reply from someone else in the room"
+		}
+	];
 });
