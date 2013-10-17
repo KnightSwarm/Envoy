978a472cd9ccec4c33783adaff5513c586daa403
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-10-17 19:04:50 +0200

    Add clarification for the Queue shim

--- src/client/Envoy Client/Resources/script/shim.js
+++ src/client/Envoy Client/Resources/script/shim.js
@@ -34,6 +34,11 @@ if(typeof has_python == "undefined")
 	/* We don't have Python support. */
 	has_python = false;
 	
+	/* First of all we'll shim the queue; this is normally used to pass data from the SleekXMPP thread
+	 * back to the JS part of the code in the main thread - however, as we don't have Python we'll
+	 * be using an API instead. This means that we don't *have* a separate thread, so don't really
+	 * need a queue either. For the sake of consistent implementation, however, the browser
+	 * backend will use a (fake) queue in the same way the TideSDK backend would. */
 	function queue_shim() { this.constructor.apply(this, arguments); }
 	queue_shim.prototype = {
 		constructor: function(arg1) {
