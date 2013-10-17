fbad30dc95dd31fe74fb95f2c69dc98d387db2dd
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-10-17 16:26:09 +0200

    Queue shim implementation and some restructuring of the includes

--- src/client/Envoy Client/Resources/script/shim.js
+++ src/client/Envoy Client/Resources/script/shim.js
@@ -1,5 +1,7 @@
 var has_tide;
+var has_python;
 
+/* First we'll check whether the TideSDK API is available. */
 if(typeof Ti !== "undefined")
 {
 	has_tide = true;
@@ -24,3 +26,35 @@ else
 		}
 	}
 }
+
+/* Now we'll check if we have Python support. If not, we'll have to fall back to an alternative
+ * XMPP implementation. */
+if(typeof has_python == "undefined")
+{
+	/* We don't have Python support. */
+	has_python = false;
+	
+	function queue_shim() { this.constructor.apply(this, arguments); }
+	queue_shim.prototype = {
+		constructor: function(arg1) {
+			this.queue = [];
+			this.check = this.do_check.bind(this);
+		},
+		set_callback: function(callback) {
+			this.callback = callback;
+		},
+		put: function(data) {
+			this.queue.push(data);
+		},
+		do_check: function(data) {
+			while(this.queue.length > 0)
+			{
+				item = this.queue.shift();
+				this.callback(item);
+			}
+		}
+	}
+	
+	var q = new queue_shim();
+}
+
