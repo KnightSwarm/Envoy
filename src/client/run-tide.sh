8d3222ad2d43ead6eafefcb0b59e02aa43b5493d
Sven Slootweg <jamsoftgamedev@gmail.com>
2014-03-20 01:22:24 +0100

    Fix for TideSDK runscript

--- src/client/run-tide.sh
+++ src/client/run-tide.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cd ~/.tidesdk/sdk/linux/1.3.1-beta/ && python tidebuilder.py -n -r "~/projects/Envoy/src/client/Envoy Client/"
+# Somehow, TideSDK has stopped running correctly after build, so we need to run it manually...
+/home/sven/.tidesdk/sdk/linux/1.3.1-beta/Envoy\ Client/Envoy\ Client/Envoy\ Client
